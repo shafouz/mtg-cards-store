d4f21174e2f9475686d9cff138a08c52cc3fb9c8
shafou <jl_macedomatos@hotmail.com>
2020-05-26 14:28:57 -0400

    Rough draft finished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("./custom")
+require("./script")
 
 import "bootstrap"
 import "../stylesheets/application"
