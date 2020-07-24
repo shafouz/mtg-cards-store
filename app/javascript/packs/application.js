e5246744931a7ff43c46d772801342ca50e13563
shafou <jl_macedomatos@hotmail.com>
2020-07-24 11:20:02 -0400

    removed wrong import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,28 +3,26 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("./script")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-import "bootstrap"
-import "../stylesheets/application"
-import '../packs/pagy.js.erb'
-
+import "bootstrap";
+import "../stylesheets/application";
+import "../packs/pagy.js.erb";
 
 document.addEventListener("turbolinks:load", () => {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+});
 
-import $ from 'jquery';
+import $ from "jquery";
 
 global.$ = jQuery;
-import { Pagy, PagyInit } from '../packs/pagy.js.erb'
-global.Pagy = Pagy
-global.PagyInit = PagyInit
+import { Pagy, PagyInit } from "../packs/pagy.js.erb";
+global.Pagy = Pagy;
+global.PagyInit = PagyInit;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
