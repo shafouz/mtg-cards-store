e60407d2eb2a662d381aec89e8b366d8a8da783f
shafou <jl_macedomatos@hotmail.com>
2020-07-28 13:37:50 -0400

    Fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
-file = File.open "./selected_keys.json"
-@selected_keys = JSON.load file
-file.close
+file = File.read("db/selected_keys.json")
+@selected_keys = JSON.parse(file)
 
 @selected_keys.each_with_index do |_i,index|
   Product.create(name: @selected_keys[index]["name"], set: @selected_keys[index]["set"], card_id: @selected_keys[index]["card_id"], image_uris: @selected_keys[index]["image_uris"], oracle_text: @selected_keys[index]["oracle_text"])
