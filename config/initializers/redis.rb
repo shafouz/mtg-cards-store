60ed67394e90510fdebc86528fdc199ed4709d7d
shafou <jl_macedomatos@hotmail.com>
2020-08-02 12:24:16 -0400

    Added .env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,3 @@
 require 'redis'
 
-Redis.current = Redis.new(url: ENV["REDIS_URL"], port: ENV["REDIS_PORT"], db: ENV["REDIS_DB"])
-if ENV["REDISCLOUD_URL"]
-  uri = URI.parse(ENV["REDISCLOUD_URL"])
-  Redis.current = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+$redis = Redis.new(url: ENV["REDIS_URL"])
