413975f9dfe0d95283d97eae657a276b0ee635b8
shafou <jl_macedomatos@hotmail.com>
2020-08-02 15:51:33 -0400

    la

--- db/migrate/20200728175128_adding_stripe_and_paypal.rb
+++
@@ -1,6 +0,0 @@
-class AddingStripeAndPaypal < ActiveRecord::Migration[6.0]
-  def change
-    add_column :products, :stripe_plan_name, :string
-    add_column :products, :paypal_plan_name, :string
-  end
-end
--- db/migrate/20200802193037_destroy_stuff.rb
+++
@@ -1,9 +0,0 @@
-class DestroyStuff < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :orders
-    remove_column :products, :stripe_plan_name
-    remove_column :products, :paypal_plan_name
-    remove_column :products, :price_cents
-    remove_column :products, :price_currency
-  end
-end
