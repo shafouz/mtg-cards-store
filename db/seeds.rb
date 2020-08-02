5f28baf727a74e9a6d5bbb189ea7a418014a4391
shafou <jl_macedomatos@hotmail.com>
2020-08-02 15:11:44 -0400

    LALal

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # "name", "set", "card_id", "image_uris", "oracle_text"
 
-sets = JSON.parse(File.read("sets.json"));
+sets = JSON.parse(File.read("db/sets.json"));
 
 sets.each do |set|
   Product.create(name: set["name"], set: set["set"], oracle_text: set["oracle_text"], card_id: set["card_id"], image_uris: set["image_uris"]) if set["set"] != "eld"
