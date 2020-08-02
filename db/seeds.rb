2649981d7bc9881760c6b1c457e9d6ba1c39f987
shafou <jl_macedomatos@hotmail.com>
2020-08-02 15:58:35 -0400

    la

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
 sets = JSON.parse(File.read("db/sets.json"));
 
 sets.each do |set|
-  Product.create(name: set["name"], set: set["set"], oracle_text: set["oracle_text"], card_id: set["card_id"], image_uris: set["image_uris"]) if set["set"] != "eld"
+  Product.new(name: set["name"], set: set["set"], oracle_text: set["oracle_text"], card_id: set["card_id"], image_uris: set["image_uris"]) if set["set"] != "eld"
   Product.update!(card_id: set["card_id"], oracle_text: set["oracle_text"]) if set["set"] == "eld"
 end
