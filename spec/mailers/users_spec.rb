c98275420effe6b1f97fb7efbe494cc5e6b35ef4
shafou <jl_macedomatos@hotmail.com>
2020-08-16 14:32:08 -0400

    failed testing

--- spec/mailers/users_spec.rb
+++ spec/mailers/users_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe User, :type => :mailer do
   describe "welcome email" do
-    let(:user) { User.create(name: 'Joao', email: 'example@example.com') }
+    let(:user) { User.create(name: 'Joao', email: 'test@test.anime') }
     let(:mail) { UserMailer.with(user: user).welcome_email.deliver_now }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Welcome to My Awesome Site")
+      expect(mail.subject).to eq("Welcome to Mtg-cards-store")
       expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["#{ENV["EMAIL"]}"])
     end
